0e8946aa53f67dbd55de7fea0b05efc7c504dfca
notrandomath <therealrandomath@gmail.com>
2022-12-27 19:16:30 -0500

    Beautified Design

--- client/src/pages/addPage/addPage.js
+++ client/src/pages/addPage/addPage.js
@@ -19,7 +19,7 @@ function AddPage() {
 
   const validationSchema = Yup.object().shape({
       title: Yup.string().required("You must input a title"),
-      rating: Yup.number("You must input a integer").integer("You must input an integer").min(0).max(10).required("You must input a number"),
+      rating: Yup.number("You must input an integer").integer("You must input an integer").min(0).max(10).required("You must input an integer"),
       img: Yup.string().required("You must input a valid image link")
   })
 
@@ -32,42 +32,42 @@ function AddPage() {
 
   return (
     <div className="addPage">
+      <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
+          <Form className="createForm">
+              <Field 
+                  id="fieldEntry"
+                  name="title"
+                  placeholder="Title"
+              />
+              <ErrorMessage name="title" component="span"/>
+              <Field 
+                  id="fieldEntry"
+                  name="rating"
+                  placeholder="Rating"
+              />
+              <ErrorMessage name="rating" component="span"/>
+              <Field 
+                  id="fieldEntry"
+                  name="img"
+                  placeholder="Image"
+              />
+              <ErrorMessage name="img" component="span"/>
+              <button type="submit">
+                  <img src="assets/add.svg" alt=""/>
+              </button>
+          </Form>
+      </Formik>
+      <div className="backButton">
+          <form action="/">
+            <button>
+                <img src="assets/back.svg" alt=""/>
+            </button>
+          </form>
+      </div>
       <div className="sampleResult">
         Sample Reults (if successful):
         <SearchResults listOfAnime={listOfAnime} setListOfAnime={setListOfAnime}/>
       </div>
-        <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
-            <Form className="createForm">
-                <Field 
-                    id="titleInput"
-                    name="title"
-                    placeholder="Title"
-                />
-                <ErrorMessage name="title" component="span"/>
-                <Field 
-                    id="ratingInput"
-                    name="rating"
-                    placeholder="Rating"
-                />
-                <ErrorMessage name="rating" component="span"/>
-                <Field 
-                    id="imgInput"
-                    name="img"
-                    placeholder="Image"
-                />
-                <ErrorMessage name="img" component="span"/>
-                <button type="submit">
-                    <img src="assets/add.svg" alt=""/>
-                </button>
-            </Form>
-        </Formik>
-        <div className="backButton">
-            <form action="/">
-              <button>
-                  <img src="assets/back.svg" alt=""/>
-              </button>
-            </form>
-          </div>
     </div>
   )
 }
