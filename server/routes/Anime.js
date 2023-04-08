9849292e6f0c0669a07930b5dd4b5219d02af158
Omar Yahia <oyahia@uci.edu>
2023-04-08 16:56:49 -0700

    fixed authentication server-end

--- server/routes/Anime.js
+++ server/routes/Anime.js
@@ -5,26 +5,11 @@ const { Op } = require("sequelize");
 const { CognitoJwtVerifier } = require("aws-jwt-verify");
 
 // Verifier that expects valid access tokens:
-const verifier = CognitoJwtVerifier.create({
+const jwtVerifier = CognitoJwtVerifier.create({
   userPoolId: "us-west-1_ouFArkh6J",
   tokenUse: "access",
   clientId: "7haritsra4ojvce0ln0p12ar66",
 });
-
-async function verifyToken(req, res) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "Missing authorization header" });
-  }
-  const token = authHeader.split(' ')[1];
-  try {
-    const token = authHeader.split(" ")[1];
-    await verifier.verify(token);
-    next();
-  } catch (err) {
-    return res.status(401).send({ message: "Invalid token", token: token, error: err });
-  }
-}
 
 //needs a json with one field called search that has some string to search for
 router.get("/:search", async (req, res) => {
@@ -39,8 +24,14 @@ router.get("/:search", async (req, res) => {
 });
 
 //needs a json with each corresponding field of the sql table
-router.post("/", verifyToken, async (req, res) => {
+router.post("/", async (req, res) => {
   const animeBody = req.body;
+  try {
+    // A valid JWT is expected in the HTTP header "authorization"
+    await jwtVerifier.verify(req.header("authorization").split(' ')[1]);
+  } catch (err) {
+    return res.status(403).json({ statusCode: 403, message: err + '' });
+  }
   await Anime.create(animeBody);
   res.json(animeBody);
 });
